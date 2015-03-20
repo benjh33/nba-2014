bcf03979728847fa840c592ce5c96e2e22f1b888
Ben Hunter <bjameshunter@gmail.com>
2015-03-19 22:23:01 -0500

    removing ggplot record chart

--- scratch.R
+++ scratch.R
@@ -1,15 +1,15 @@
-library(bbscrapeR)
-library(htmlwidgets)
-library(htmltools)
-library(dplyr)
-library(ggd3)
-library(ggplot2)
-library(stringr)
-library(doMC)
-library(MASS)
-
-registerDoMC(4)
-
-data(players)
-data(nba_wl_games)
-
+# library(bbscrapeR)
+# library(htmlwidgets)
+# library(htmltools)
+# library(dplyr)
+# library(ggd3)
+# library(ggplot2)
+# library(stringr)
+# library(doMC)
+# library(MASS)
+#
+# registerDoMC(4)
+#
+# data(players)
+# data(nba_wl_games)
+#
